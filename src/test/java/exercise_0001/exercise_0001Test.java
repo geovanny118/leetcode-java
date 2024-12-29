a973d7c604f4b0deaffe4fb74f6215912d038738
geovanny118 <geovanny118@hotmail.com>
2024-12-28 22:43:42 -0500

    Create exercise_0001Test.java
    Unit Tests are added to exercise 1

---
+++ src/test/java/exercise_0001/exercise_0001Test.java
@@ -0,0 +1,34 @@
+package exercise_0001;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static exercise_0001.exercise_0001.TwoSum;
+import static org.junit.jupiter.api.Assertions.*;
+
+class exercise_0001Test {
+
+    @Test
+    void testCase1() {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        assertArrayEquals(new int[] { 0, 1 }, TwoSum(nums, target));
+    }
+
+    @Test
+    void testCase2() {
+        int[] nums = { 3, 2, 4 };
+        int target = 6;
+
+        assertArrayEquals(new int[] { 1, 2 }, TwoSum(nums, target));
+    }
+
+    @Test
+    void testCase3() {
+        int[] nums = { 3, 3 };
+        int target = 6;
+
+        assertArrayEquals(new int[] { 0, 1 }, TwoSum(nums, target));
+    }
+}
