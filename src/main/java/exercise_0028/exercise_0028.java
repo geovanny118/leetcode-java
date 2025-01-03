cf2c98795cb5f14de8b294a6aede17ffa93384d9
geovanny118 <geovanny118@hotmail.com>
2025-01-02 22:56:15 -0500

    Solved exercise 0028
    Create and solved exercise 28

---
+++ src/main/java/exercise_0028/exercise_0028.java
@@ -0,0 +1,7 @@
+package exercise_0028;
+
+public class exercise_0028 {
+    public static int strStr(String haystack, String needle) {
+        return (haystack.contains(needle)) ? haystack.indexOf(needle) : -1;
+    }
+}
