34ee2922c6c8d59be64ddbbc0dbc31ad7db859be
Geovanni Rojas <geovanny118@hotmail.com>
2025-01-11 15:22:34 -0500

    Refactoring
    Refactoring for solution to java

--- src/main/java/exercise_0125/exercise_0125.java
+++ src/main/java/exercise_0125/exercise_0125.java
@@ -1,40 +1,40 @@
 package exercise_0125;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class exercise_0125 {
-    public boolean IsPalindrome(string s) {
-        s = PrepareString(s);
-        if(s.Equals(ReverseString(s)))
-        {
-            return true;
+    public boolean isPalindrome(String s) {
+        s = prepareString(s);
+        return s.equals(reverseString(s));
+    }
+
+    public String prepareString(String s) {
+        s = removePunctuationAndSpaces(s);
+        s = changeToLowerCase(s);
+        return s;
+    }
+
+    public String removePunctuationAndSpaces(String s) {
+        String pattern = "[.,:;!?@#_'|\\\\/\\- {}\"\\[\\]()`]";
+        String substitution = "";
+        return s.replaceAll(pattern, substitution);
+    }
+
+    public static String changeToLowerCase(String s) {
+        Pattern pattern = Pattern.compile("[A-Z]");
+        Matcher matcher = pattern.matcher(s);
+
+        StringBuffer result = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(result, matcher.group().toLowerCase());
         }
-        return false;
+        matcher.appendTail(result);
+
+        return result.toString();
     }
 
-    public String PrepareString(string s)
-    {
-        s = RemovePunctuationAndSpaces(s);
-        s = ChangeToLowerCase(s);
-        return s;
-    }
-
-    public String RemovePunctuationAndSpaces(string s)
-    {
-        string pattern = @"[.,:;!?@#_'|\\/\- {}\""[\]()``]";
-        string subtitution = "";
-        s = Regex.Replace(s, pattern, subtitution);
-        return s;
-    }
-
-    public String ChangeToLowerCase(string s)
-    {
-        s = Regex.Replace(s, @"[A-Z]", match => match.Value.ToLower());
-        return s;
-    }
-
-    public String ReverseString(string s)
-    {
-        char[] charArray = s.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
+    public String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
     }
 }
---
+++ src/test/java/exercise_0125/exercise_0125Test.java
@@ -0,0 +1,7 @@
+package exercise_0125;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class exercise_0125Test {
+
+}
